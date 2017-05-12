e2ce2fda01e76a70577cdc54c44690f511eb65c2
cautree <yanyanliujs@gmail.com>
2017-05-12 11:16:57 -0400

    python, peewee and flask

---
+++ students.py
@@ -0,0 +1,31 @@
+from peewee import *
+
+
+db = MySQLDatabase('students',host="localhost", port=3306)
+
+
+class Student(Model):
+    """A base model that will use our MySQL database"""
+    username = CharField(max_length=255, unique=True)
+    points = IntegerField(default=0)
+
+    class Meta:
+        database = db
+
+
+
+def get_all_students():
+	students = Student.select()
+
+def top_student():
+	student = Student.select().order_by(Student.points.desc()).get()
+	return student
+
+
+
+
+if __name__ =='__main__':
+	db.connect()
+	db.create_tables([Student], safe=True)
+	#add_students()
+	print ("Our top student right now is: {0.username}.".format(top_student()))
