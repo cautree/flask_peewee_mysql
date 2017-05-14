c0e3bc7770a3d70687f02ff0e82f3fb1bc60f1ac
cautree <yanyanliujs@gmail.com>
2017-05-14 13:01:44 -0400

    add create post

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, g,render_template, flash, redirect, url_for
 from flask.ext.bcrypt import check_password_hash
-from flask.ext.login import LoginManager, login_user, logout_user, login_required
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
 
 import models
 import forms
@@ -31,6 +31,7 @@ def before_request():
 	g.db = models.DATABASE
 	#g.db.connect()
 	g.db.get_conn()
+	g.user = current_user
 
 @app.after_request
 def after_request(response):
@@ -77,21 +78,50 @@ def logout():
 	flash("You've been logged out!", "success")
 	return redirect(url_for('index'))
 
+@app.route('/new_post', methods =('GET','POST'))
+@login_required
+def post():
+	form = forms.PostForm()
+	if form.validate_on_submit():
+		models.Post.create(user=g.user._get_current_object(),
+			content= form.content.data.strip())
+		flash("Message posted! Thanks!", "success")
+		return redirect(url_for('index'))
+	return render_template('post.html', form=form)
+
 @app.route('/')
 def index():
-	return 'Hey'
+	stream = models.Post.select().limit(100)
+	return render_template('stream.html', stream=stream)
+
+
+@app.route('/stream')
+@app.route('/stream/<username>')
+def stream(username = None):
+	template = 'stream.html'
+	if username and username != current_user.username:
+		user = models.User.select().where(models.User.username**username).get()
+		stream = user.posts.limit(100)
+	else:
+		stream = current_user.get_stream().limit(100)
+		user = current_user
+
+	if username:
+		template ='user_stream.html'
+	return render_template(template, stream=stream, user=user)
 
 
 
 if __name__ == '__main__':
 	models.initialize()
 	try:
-		models.User.create_user(
-			username = 'Yanyan',
-			email = 'yanyan@example.com',
-			password = 'password',
-			admin = True
-			)
+		with models.DATABASE.transaction():
+			models.User.create_user(
+				username = 'Yanyan',
+				email = 'yanyan@example.com',
+				password = 'password',
+				admin = True
+				)
 	except ValueError:
 		pass
 	#app.run(debug=DEBUG, host= HOST, port = PORT)
